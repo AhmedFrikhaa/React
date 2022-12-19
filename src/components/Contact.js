ae37e09ae22aa6d789fbbbdf36cb32805e3a2636
AhmedFrikhaa <ahmed.frikha2@insat.ucar.tn>
2022-12-19 20:07:25 +0100

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+
+export default function Contact(){
+    return(
+        <div>
+            <h2>
+                Ahmed Frikha
+            </h2>
+            <p>
+                ahmedfrikha59@gmail.com
+            </p>
+        </div>
+    )
+}
---
+++ src/oldApp.js
@@ -0,0 +1,35 @@
+import Contact from "./components/Contact";
+
+function App() {
+    const contacts=[
+        {id:1 , fullname:"Ahmed Frikha", email:"ahmedfrikha1@gmail.com"} ,
+        {id:2 , fullname:"Ahmed Frikha", email:"ahmedfrikha2@gmail.com"} ,
+        {id:3 , fullname:"Ahmed Frikha", email:"ahmedfrikha3@gmail.com"} ,
+    ];
+
+    /*const  element=<h1>Bonjour</h1>
+    return (
+       <div className="App">
+         {element}
+         <Contact fullname="Ahmed Frikha" email="ahmedfrikha59@gmail.com"></Contact>
+         <Contact fullname="Ahmed Frikha" email={"ahmed.frikha2@insat.ucar.tn"}></Contact>
+         <Contact/>
+       </div>
+     );*/
+    return(<div className={App}>
+
+        {
+
+
+            contacts.map((c)=>{
+                return(
+                    <Contact key={c.id} fullname={c.fullname} email={c.email} />
+
+                );
+            })
+        }
+        <Contact></Contact>
+    </div>)
+}
+
+export default App;
